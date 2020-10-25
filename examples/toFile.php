9384e65147ccebf0a61150bb739849f39adb7faa
Jiri Kyncl <jiri.kyncl@movisio.com>
2020-10-25 16:24:04 +0100

    Better examples

--- examples/toFile.php
+++ examples/toFile.php
@@ -9,9 +9,8 @@ require_once '../src/SvgGenerator.php';
 
 
 $chords = [
-    [["o","x","","o","","",""],["","","","","1","",""],["","","","3","","",""],["","","","","","","IV"],["","5","","","","",""],["","","","","","",""]],
-    [["","","","x","","",""],["","","","","","",""],["","","4","","5","",""],["","","","","","","III"],["","","","","","",""],["","","","","","",""]],
-    [["","","","x","","o",""],["","","","","1","",""],["","","2","","","",""],["","","","3","","","V"],["","","","","","",""],["","","","","","","VII"]]
+    [["x","","","o","","x",""],["","","","","1","",""],["","","2","","","",""],["","3","","","","",""],["","","","","","",""],["","","","","","",""]],
+    [["","","o","o","o","",""],["","","","","","",""],["","1","","","","",""],["2","","","","","3",""],["","","","","","",""],["","","","","","",""]],
 ];
 
 $svgGenerator = new SvgGenerator();
@@ -21,13 +20,8 @@ $chord1 = $svgGenerator->renderSvg($chords[0]);
 
 $svgGenerator->setSize(200);
 $svgGenerator->setBackgroundColor('yellow');
-$chord2 = $svgGenerator->renderSvg($chords[1]);
-
-$svgGenerator->setSize(500);
-$svgGenerator->setBackgroundColor('pink');
 $svgGenerator->setColor('red');
-$chord3 = $svgGenerator->renderSvg($chords[2]);
+$chord2 = $svgGenerator->renderSvg($chords[1]);
 
 file_put_contents('chord1.svg', $chord1);
 file_put_contents('chord2.svg', $chord2);
-file_put_contents('chord3.svg', $chord3);
